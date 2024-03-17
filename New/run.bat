c78cabbbb2a1723e61ac51d45ac8efc684fbc9d9
tetroner <omkadam2125@gmail.com>
2024-03-17 16:38:42 +0530

    Update run.bat to download python.ps1 and files.bat from GitHub

--- New/run.bat
+++ New/run.bat
@@ -1,5 +1,5 @@
-curl -O -f 
+curl -O -f https://raw.githubusercontent.com/Tetroner9/CSI/main/New/python.ps1
 
-curl -O -f
+curl -O -f https://raw.githubusercontent.com/Tetroner9/CSI/main/New/files.bat
 
 powershell.exe -noprofile -executionpolicy bypass -file .\python.ps1
